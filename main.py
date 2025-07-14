160a481a3f0d7efcdfdc98267904d7deec04f5f0
user <user@Astrology-Application.lan>
2025-07-14 14:13:37 +0000

    trying to fix port

--- main.py
+++ main.py
@@ -56,7 +56,5 @@ def zodiac_handler():
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
-# ✅ Required for Cloud Run to work
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8080)
